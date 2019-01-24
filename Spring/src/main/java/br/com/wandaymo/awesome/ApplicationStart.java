86cb3263d878b9f9e6a73af1e680ebe7400def6a
Wandaymo <wandaymo@Wandaymo-PC.Wandaymo-PC>
2019-01-24 17:13:43 -0300

    002

---
+++ Spring/src/main/java/br/com/wandaymo/awesome/ApplicationStart.java
@@ -0,0 +1,17 @@
+package br.com.wandaymo.awesome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//Substitui todas as importações inseridas abaixo
+@SpringBootApplication
+
+//@EnableAutoConfiguration
+//@ComponentScan
+//@Configuration
+public class ApplicationStart {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStart.class, args);
+    }
+}
---
+++ Spring/src/main/java/br/com/wandaymo/awesome/endpoint/StudentEndPoint.java
@@ -0,0 +1,27 @@
+package br.com.wandaymo.awesome.endpoint;
+
+
+import br.com.wandaymo.awesome.model.Student;
+import br.com.wandaymo.awesome.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+@RestController
+@RequestMapping("student")
+public class StudentEndPoint {
+    //Configura as dependências e instacia o objeto
+    @Autowired
+    private DateUtil dateUtil;
+    @RequestMapping(method = RequestMethod.GET, path = "/list")
+    public List<Student> listALL(){
+        //System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return asList(new Student("Wandaymo"), new Student("Radynalva"));
+    }
+}
